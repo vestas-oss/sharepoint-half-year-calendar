c694ca024d9384a9987a1a2bd7008730f07b3091
Allan Hvam <allanhp@hotmail.com>
2024-07-30 09:18:49 +0200

    feat: use StartDate field

--- packages/webpart/src/event-sources/sharepoint.ts
+++ packages/webpart/src/event-sources/sharepoint.ts
@@ -1,5 +1,6 @@
 import { IList } from "@pnp/sp/lists";
 import "@pnp/sp/forms";
+import "@pnp/sp/fields";
 import { useSharePoint } from "../hooks/useSharePoint";
 import { Event } from "../types/Event";
 
@@ -29,31 +30,34 @@ export const sharepoint = {
             calendar = lists.getById(listInfos[0].Id);
         }
 
+        const formsPromise = calendar.forms.filter("FormType eq 4")();
+        const fieldsPromise = calendar.fields.filter("TypeAsString eq 'DateTime'").select("InternalName")();
+        const [forms, fields] = await Promise.all([formsPromise, fieldsPromise]);
+
         type SharePointEvent = {
             ID: number;
             Title: string;
-            EventDate: string;
             EndDate: string;
-        };
+        } & ({ EventDate: string; } | { StartDate: string; });
 
         let items = new Array<SharePointEvent>();
         const isoString = (date: Date) => date.toISOString().split(".").shift() + "Z";
-        const dateFilter = (s: "Event" | "End") =>
+        const dateFilter = (s: "Event" | "Start" | "End") =>
             `${s}Date ge datetime'${isoString(query.start)}' and ` +
             `${s}Date le datetime'${isoString(query.end)}'`;
         // Filter: starts or ends within period
-        const calendarFilter = `(${dateFilter("Event")}) or (${dateFilter("End")})`;
-        const selects = ["ID", "Title", "EventDate", "EndDate"];
+        const startFieldName = fields.find(f => f.InternalName === "StartDate") ? "Start" : "Event";
+        const calendarFilter = `(${dateFilter(startFieldName)}) or (${dateFilter("End")})`;
+        const selects = ["ID", "Title", `${startFieldName}Date`, "EndDate"];
         for await (const page of calendar.items.filter(calendarFilter).select(...selects)) {
             items = items.concat(page);
         }
 
-        const forms = await calendar.forms.filter("FormType eq 4")();
         const formUrl = forms[0]?.ServerRelativeUrl;
 
         const events = items
             .map((item) => {
-                const start = item.EventDate;
+                const start = item[`${startFieldName}Date`];
                 const end = item.EndDate;
                 const link = formUrl ? `${formUrl}?ID=${item.ID}&Source=${encodeURI(window.location.href)}` : undefined;
                 return {
